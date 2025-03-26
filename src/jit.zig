e4a170acc843bd5d6ffe70613aa4c2f5038aa1f3
Ahmed Moalla <ahmed.moalla@gmail.com>
2025-03-26 22:27:57 +0100

    use stack instead of backpatches to handle jumping between [ and ]

--- src/jit.zig
+++ src/jit.zig
@@ -23,10 +23,10 @@ pub const JittedCode = struct {
 pub fn compile(allocator: std.mem.Allocator, ops: []lexer.Op) !JittedCode {
     var code = std.ArrayList(u8).init(allocator);
     defer code.deinit();
-    var backpatches = std.ArrayList(Backpatch).init(allocator);
-    defer backpatches.deinit();
     var addresses = std.ArrayList(usize).init(allocator);
     defer addresses.deinit();
+    var jump_tbl = std.ArrayList(usize).init(allocator);
+    defer jump_tbl.deinit();
 
     for (ops) |op| {
         try addresses.append(code.items.len);
@@ -36,10 +36,6 @@ pub fn compile(allocator: std.mem.Allocator, ops: []lexer.Op) !JittedCode {
                     return error.OperandTooBig;
                 }
                 try code.appendSlice(&[_]u8{ 0x80, 0x07, @intCast(op.operand & 0xFF) }); // add byte[rdi], operand
-                // for (code.items) |byte| {
-                //     std.debug.print("{x}", .{byte});
-                // }
-                // std.process.exit(0);
             },
             .dec => {
                 if (op.operand > 255) {
@@ -49,72 +45,55 @@ pub fn compile(allocator: std.mem.Allocator, ops: []lexer.Op) !JittedCode {
             },
             .left => {
                 try code.appendSlice(&[_]u8{ 0x48, 0x81, 0xef }); // sub rdi,
-                // var bytes: [4]u8 = undefined;
-                // std.mem.writeInt(u32, &bytes, @intCast(op.operand), .little);
                 try code.appendSlice(std.mem.sliceAsBytes(&[_]u32{@intCast(op.operand)})); // operand
             },
             .right => {
                 try code.appendSlice(&[_]u8{ 0x48, 0x81, 0xc7 }); // add rdi,
-                // var bytes: [4]u8 = undefined;
-                // std.mem.writeInt(u32, &bytes, @intCast(op.operand), .little);
                 try code.appendSlice(std.mem.sliceAsBytes(&[_]u32{@intCast(op.operand)})); // operand
             },
             .output => {
                 for (0..op.operand) |_| {
-                    try code.append(0x57); // push rdi
-                    try code.appendSlice(&[_]u8{ 0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00 }); // mov rax, 1
-                    try code.appendSlice(&[_]u8{ 0x48, 0x89, 0xfe }); // mov rsi, rdi
-                    try code.appendSlice(&[_]u8{ 0x48, 0xc7, 0xc7, 0x01, 0x00, 0x00, 0x00 }); // mov rdi, 1
-                    try code.appendSlice(&[_]u8{ 0x48, 0xc7, 0xc2, 0x01, 0x00, 0x00, 0x00 }); // mov rdx, 1
-                    try code.appendSlice(&[_]u8{ 0x0f, 0x05 }); // syscall
-                    try code.append(0x5f); // pop rdi
+                    try code.appendSlice(&[_]u8{
+                        0x57, // push rdi
+                        0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00, // mov rax, 1
+                        0x48, 0x89, 0xfe, // mov rsi, rdi
+                        0x48, 0xc7, 0xc7, 0x01, 0x00, 0x00, 0x00, // mov rdi, 1
+                        0x48, 0xc7, 0xc2, 0x01, 0x00, 0x00, 0x00, // mov rdx, 1
+                        0x0f, 0x05, // syscall
+                        0x5f, // pop rdi
+                    });
                 }
             },
-            .input => unreachable,
+            .input => {},
             .jump_if_zero => {
-                try code.appendSlice(&[_]u8{ 0x8a, 0x07 }); // mov al, byte [rdi]
-                try code.appendSlice(&[_]u8{ 0x84, 0xc0 }); // test al, al
-                try code.appendSlice(&[_]u8{ 0x0f, 0x84 }); // jz
-                const operand_byte_addr = code.items.len;
-                try code.appendNTimes(0, 4); // 4 bytes address to override with backpatches
-                const src_byte_addr = code.items.len;
-
-                try backpatches.append(Backpatch{
-                    .operand_byte_addr = operand_byte_addr,
-                    .src_byte_addr = src_byte_addr,
-                    .dst_op_index = op.operand,
+                try code.appendSlice(&[_]u8{
+                    0x8a, 0x07, // mov al, byte [rdi]
+                    0x84, 0xc0, // test al, al
+                    0x0f, 0x84, // jz
+                    0x0,  0x0,
+                    0x0, 0x0, // 4 bytes address when we reach ']'
                 });
+                try jump_tbl.append(code.items.len);
             },
             .jump_if_nonzero => {
-                try code.appendSlice(&[_]u8{ 0x8a, 0x07 }); // mov al, byte [rdi]
-                try code.appendSlice(&[_]u8{ 0x84, 0xc0 }); // test al, al
-                try code.appendSlice(&[_]u8{ 0x0f, 0x85 }); // jnz
-                const operand_byte_addr = code.items.len;
-                try code.appendNTimes(0, 4); // 4 bytes address to override with backpatches
-                const src_byte_addr = code.items.len;
-
-                try backpatches.append(Backpatch{
-                    .operand_byte_addr = operand_byte_addr,
-                    .src_byte_addr = src_byte_addr,
-                    .dst_op_index = op.operand,
+                try code.appendSlice(&[_]u8{
+                    0x8a, 0x07, // mov al, byte [rdi]
+                    0x84, 0xc0, // test al, al
+                    0x0f, 0x85, // jnz
+                    0x0, 0x0, 0x0, 0x0, // 4 bytes address filled from jumb_tbl
                 });
+
+                const left = jump_tbl.pop().?;
+                const right = code.items.len;
+                const offset: i32 = @intCast(right - left);
+
+                @memcpy(code.items[left - 4 .. left], &std.mem.toBytes(offset));
+                @memcpy(code.items[right - 4 .. right], &std.mem.toBytes(-offset));
             },
         }
     }
 
     try addresses.append(code.items.len);
-
-    for (backpatches.items) |patch| {
-        const src_addr: i32 = @intCast(patch.src_byte_addr);
-        const dst_addr: i32 = @intCast(addresses.items[patch.dst_op_index]);
-        const operand: i32 = dst_addr - src_addr;
-
-        const bytes = std.mem.sliceAsBytes(&[_]i32{operand});
-        code.items[patch.operand_byte_addr] = bytes[0];
-        code.items[patch.operand_byte_addr + 1] = bytes[1];
-        code.items[patch.operand_byte_addr + 2] = bytes[2];
-        code.items[patch.operand_byte_addr + 3] = bytes[3];
-    }
 
     try code.append(0xC3); // ret
 
@@ -122,12 +101,13 @@ pub fn compile(allocator: std.mem.Allocator, ops: []lexer.Op) !JittedCode {
     const jitted = try std.posix.mmap(
         null,
         content.len,
-        std.posix.PROT.EXEC | std.posix.PROT.READ | std.posix.PROT.WRITE,
+        std.posix.PROT.READ | std.posix.PROT.WRITE,
         .{ .TYPE = .PRIVATE, .ANONYMOUS = true },
         -1,
         0,
     );
     @memcpy(jitted[0..content.len], content);
 
+    try std.posix.mprotect(jitted, std.posix.PROT.READ | std.posix.PROT.EXEC);
     return JittedCode{ .machine_code = jitted };
 }
